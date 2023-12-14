7067dd180344d2b6ecdc57eb59e4caa686c71aed
William Paddison <138761963+wpaddiso999@users.noreply.github.com>
2023-12-14 13:02:50 -0500

    Update movies.py

--- movies.py
+++ movies.py
@@ -3,13 +3,15 @@ class Movies:
         self._movies = []
 
         with open(movies_file, encoding="utf-8") as file:
-            row_idx = 0
-            for line in file:
-                if row_idx%3 == 0:
+            movie_name = None
+            movie_cast = None
+
+            for row_idx, line in enumerate(file):
+                if row_idx % 3 == 0:
                     movie_name = line.rstrip()
-                if row_idx%3 == 1:
+                elif row_idx % 3 == 1:
                     movie_cast = line.rstrip().split(',')
-                if row_idx%3 == 2:
+                elif row_idx % 3 == 2:
                     self._movies.append(
                         {
                             'name': movie_name,
@@ -18,16 +20,28 @@ class Movies:
                     )
                     movie_name = None
                     movie_cast = None
-                row_idx += 1
 
-        if movie_name and movie_cast:
-            # Add the last movie to the list
-            self._movies.append(
-                {
-                    'name': movie_name,
-                    'cast': movie_cast
-                }
-            )
+            if movie_name and movie_cast:
+                # Add the last movie to the list
+                self._movies.append(
+                    {
+                        'name': movie_name,
+                        'cast': movie_cast
+                    }
+                )
 
-if __name__ == "__main__":
-    movies = Movies('./movies.txt')
+    def get_movie_names(self):
+        return [movie['name'] for movie in self._movies]
+
+    def search_movies_by_name(self, keyword):
+        return [movie['name'] for movie in self._movies if keyword.lower() in movie['name'].lower()]
+
+    def search_movies_by_cast(self, keyword):
+        result = []
+        for movie in self._movies:
+            if any(keyword.lower() in cast.lower() for cast in movie['cast']):
+                result.append(movie['name'])
+        return result
+
+    def get_all_movies(self):
+        return self._movies
